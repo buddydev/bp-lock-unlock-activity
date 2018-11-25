a5ca9b7f818cb8191d584fb3d63a7e28da6c7e73
Brajesh Singh <brajesh@cosmiccoders.com>
2018-11-25 06:40:58 +0530

    Update version

--- bp-lock-unlock-activity.php
+++ bp-lock-unlock-activity.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: BuddyPress Lock Unlock Activity
- * Version: 1.0.4
+ * Version: 1.0.5
  * Plugin URI: https://buddydev.com/plugins/bp-lock-unlock-activity/
  * Author: BuddyDev
  * Author URI: https://buddydev.com
